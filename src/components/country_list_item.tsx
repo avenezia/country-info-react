13b39ff3f802a10e74b6eb7b43894ee808b57299
avenezia <>
2018-07-19 17:02:54 +0200

    Adding digit formatting

--- src/components/country_list_item.tsx
+++ src/components/country_list_item.tsx
@@ -4,8 +4,8 @@ export function CountryListItem(props: any) {
     return (
         <tr key={props.name}>
             <td className="align-middle">{props.name}</td>
-            <td className="align-middle">{props.population}</td>
-            <td className="align-middle">{props.area}</td>
+            <td className="align-middle">{new Intl.NumberFormat().format(props.population)}</td>
+            <td className="align-middle">{new Intl.NumberFormat().format(props.area)}</td>
             <td className="align-middle">{props.capital}</td>
             <td><img src={props.flag}/></td>
         </tr>
